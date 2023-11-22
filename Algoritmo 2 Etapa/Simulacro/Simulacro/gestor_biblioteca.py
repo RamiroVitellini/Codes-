debf431d4d81ad34f000e0a51d6f96a0047336ca
RamiroVitellini <150476851+RamiroVitellini@users.noreply.github.com>
2023-11-22 18:00:57 -0300

    Update gestor_biblioteca.py

--- Algoritmo 2 Etapa/Simulacro/Simulacro/gestor_biblioteca.py
+++ Algoritmo 2 Etapa/Simulacro/Simulacro/gestor_biblioteca.py
@@ -1,67 +1,100 @@
 from clase_libro import *
-class GestorBiblioteca:
+
+class Gestor_Libros:
     def __init__(self):
-        self.lista_libros : list[Libro] =[]
-
-    def verificar_id(self, id):
-        while True:
-            for id_existentes in self.lista_libros:
-                if id_existentes.id == id:
-                    print ("Ese id ya existe")
-                return id
-
-    def verificar_estado(self):
-            self.estado_libros = self.devolver_alquilado()
-
-    def elegir_tipo(self):
-        lista_tipos = []
-        for i in Libro.__subclasses__():
-            print(f"-{i.__name__}")
-            lista_tipos.append(i.__name__)
-        while True:
-            tipo = input("elija el tipo de libro que desea crear: ")
-            if tipo in lista_tipos:
-                return str(tipo)
-            else:
-                print("este tipo de libro no es valido...")
-
-    idiomas = {
+        self.lista_libros : list[Libro] = []
+        self.idiomas_disponibles = {
     "IN":"ingles", 
     "AL":"aleman",
     "CH":"chino"
     }
 
-    def crear_libros(self):
-        id = self.verificar_id(input("Ingrese el id del libro"))
-        nombre = input("Ingrese el nombre del libros: ")
-        autor = input("Ingrese el autor del libros: ")
-        tipo = self.elegir_tipo
-        if tipo == "LibroNiño":
-            Libro = LibroNiños(id, nombre, autor)
-            self.lista_libros.append(Libro)
-        elif tipo == "LibroIdioma":
-            Libro = LibroIdiomas(id, nombre, autor)
-            self.lista_libros.append(Libro)
-        self.loguear(texto)
-        texto = f'{id} {nombre} {autor}'
+    def crear_libro(self):
+        id = self.validar_id()
+        nombre = pedir_str('Ingrese el nombre del libro: ')
+        autor = pedir_str('Ingrese el autor del libro: ')
+        tipo = self.verificar_tipo()
+        if tipo == Libro:
+            libro = tipo(id, nombre, autor)
+            self.lista_libros.append(libro)
+        elif tipo == LibroNiño:
+            libro = tipo (id, nombre, autor, estado = False, edad_minima = pedir_int('Ingrese la edad minima: '))
+            self.lista_libros.append(libro)
+        elif tipo == IdiomaLibro:
+            libro = tipo (id, nombre, autor, estado = False, idioma = self.validar_idioma)
+            self.lista_libros.append(libro)
+    
+    
+    def validar_id(self):
+        while True: 
+            id = pedir_int("Ingrese el id del libro: ")
+            for i in self.lista_libros:
+                if i.id == id:
+                    print ("Ese id ya existe")
+            return id
+        
+    def verificar_tipo(self):
+        print("-- LISTA LIBROS --")
+        lista_tipos = []
+        for i in Libro.__subclasses__():
+            print(f"-{i.__name__}")
+            lista_tipos.append(i.__name__)
+        opcion = input("Ingrese el tipo de libro: ")
+        if opcion in lista_tipos:
+            return opcion
+        else:
+            print("El tipo de libro no existe!")
+    
+    def validar_idioma(self):
+        for i in self.idiomas_disponibles.keys():
+            print(i, end = ' - ')
+        idioma = pedir_str('Ingrese el idioma del libro: ')
+        return self.idiomas_disponibles.get(idioma, 'espanol')
 
-    def loguear(self,texto):
+    def logear(self):
         try:
-            fichero = open('archivo.txt','a+')
-            fichero.write(texto+'\n')
+            fichero = open('log.txt','a+')
+            fichero.write(f'ID: {Libro.id}, Nombre: {Libro.nombre}, Autor: {Libro.autor}\n')
             fichero.close()
         except Exception as e:
-            print(e)
+            print(e)      
 
     def listar_libros(self):
         for i in self.lista_libros:
-            print (f"Id libro: {i.id}, nombre:{i.nombre}, estado:{i.estado}")
+            if i.estado:
+                i.presentar()
 
     def cambiar_estado(self):
-        id = input ("Ingrese el id del libro para cambiar su estado")
-        if id.verficar_id():
-            cambiar_estado = input("Ingrese si quiere que este alquilado o para alquilar")
-        if cambiar_estado == ("Alquilado"):
-            id.devolver_alquilado(self) 
-        elif cambiar_estado == ("Disponible"):	
-            id.alquilar_libro(self)
+        while True:
+            id = pedir_int('Ingrese el ID del libro: ')
+            for i in self.lista_libros:
+                if i.id == id:
+                    cambiar_estado = input("Ingrese si quiere que este -Alquilado o -Para alquilar")
+                    if cambiar_estado == ("Alquilado"):
+                        id.alquilar(self) 
+                    elif cambiar_estado == ("Disponible"):	
+                        id.alquilado(self)
+            
+def pedir_int(text):
+    while True:
+        try:
+            numero = int(input(text))
+            return numero
+        except Exception as e:
+            print(f'Error: {e}. Ingrese un dato valido')
+            
+def pedir_float(text):
+    while True:
+        try:
+            numero = float(input(text))
+            return numero
+        except Exception as e:
+            print(f'Error: {e}. Ingrese un dato valido')
+            
+def pedir_str(text):
+    while True:
+        try:
+            numero = str(input(text))
+            return numero
+        except Exception as e:
+            print(f'Error: {e}. Ingrese un dato valido')
