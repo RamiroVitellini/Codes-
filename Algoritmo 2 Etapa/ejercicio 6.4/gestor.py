d0987c74c03a5c1aa87241dec0b66e0673ee6f61
RamiroVitellini <150476851+RamiroVitellini@users.noreply.github.com>
2023-11-22 12:01:07 -0300

    Update gestor.py

--- Algoritmo 2 Etapa/ejercicio 6.4/gestor.py
+++ Algoritmo 2 Etapa/ejercicio 6.4/gestor.py
@@ -7,31 +7,27 @@ Crear clase GestorAutos que cuente con las siguientes funciones para un menu
 
 from clase_vehiculos import * 
 class GestorVehiculos:
-
+    
     def __init__(self):
         self.lista_vehiculos : list[Vehiculos] = []
-    
-    def pedir_tipo(self): 
-        while True:
-            print ("----AUTO A CREAR----")
-            lista_tipos = []
-            for i in Vehiculos.__subclasses__():
-                print(f'- {i.__name__}')
-                lista_tipos.append(i.__name__)
-            tipo_clase = input('indique el auto: ')
-            if tipo_clase in lista_tipos:
-                return tipo_clase
-            else:
-                print('Esa clase no existe')
     
     def crear_auto(self):
         patente = input('Ingrese la patente: ')
         marca = input('Ingrese la marca: ')
         anio = input('Ingrese el año del modelo: ')
         origen = input('Ingrese el origen del vehiculo: ')
-        vehiculo = vehiculo(patente,marca,anio,origen)
-        self.lista_vehiculos.append(vehiculo)
-
+        velocidad = pedir_int ("Ingrese la velocidad del vehiculo: ")
+        tipo = self.validar_tipo ()
+        if tipo == Particular:
+            uso = input("Cuanto tiempo de uso tiene: ")
+            vehiculo = tipo(patente, marca, anio, origen,velocidad, uso)
+            self.lista_vehiculos.append(vehiculo)
+        elif tipo == PickUp:
+            vehiculo =  tipo(patente, marca, anio, origen,velocidad, capacidad_carga = input ("Ingrese la capacidad de carga"))
+            self.lista_vehiculos.append(vehiculo)
+        elif tipo == Deportivo:
+            vehiculo =  tipo(patente, marca, anio, origen,velocidad, caballos_fuerza = input ("Ingrese los caballos fuerza"))
+            self.lista_vehiculos.append(vehiculo)
     def listar_vehiculo(self): 
         for vehiculo in self.lista_vehiculos:
             vehiculo.get_tipo() 
@@ -43,6 +39,45 @@ class GestorVehiculos:
                 nueva_velocidad = input('Indique la nueva velocidad: ')
                 vehiculo.cambiar_velocidad(nueva_velocidad)  
 
-    def calcular_velocidad():
-        pass
-        
+    def calcular_tiempo_de_viaje(self):
+        patente = input ("Ingrese la patente del auto: ")
+        for i in self.lista_vehiculos:
+            if i.patente == patente:
+                km = pedir_int("Cuantos km recorrerá: ")
+                print (f' El tiempo en recorrer será de:' (km / i.velocidad))
+           
+    def validar_tipo(self):
+        tipos_vehiculos = {'vehiculo particular':Particular, 'vehiculo PickUp':PickUp, 'vehiculo Deportivo':Deportivo}
+        while True:
+            for tipos in tipos_vehiculos.keys():
+                print(tipos, end = ' - ')
+            tipo_elegido = pedir_str('Ingrese el tipo de vehiculo: ')
+            if tipo_elegido in tipos_vehiculos:
+                return tipos_vehiculos[tipo_elegido]
+            else:
+                print(f'El tipo de vehiculo {tipo_elegido} no existe')
+    
+
+def pedir_int(text):
+    while True:
+        try:
+            numero = int(input(text))
+            return numero
+        except Exception as e:
+            print(f'Error: {e}. Ingrese un dato valido')
+            
+def pedir_float(text):
+    while True:
+        try:
+            numero = float(input(text))
+            return numero
+        except Exception as e:
+            print(f'Error: {e}. Ingrese un dato valido')
+            
+def pedir_str(text):
+    while True:
+        try:
+            numero = str(input(text))
+            return numero
+        except Exception as e:
+            print(f'Error: {e}. Ingrese un dato valido')
